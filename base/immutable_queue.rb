190d506245c5ebe9447f9236dd6fa170ed0d9e61
Rune Funch Søltoft <funchsoltoft@gmail.com>
2013-04-06 09:55:36 +0200

    trying to fix erors due to changes in dependencies

--- base/immutable_queue.rb
+++ base/immutable_queue.rb
@@ -1,46 +1,48 @@
 context :ImmutableQueue do
-  role :front do end
-  role :back do end
+  role :front do
+  end
+  role :back do
+  end
   push do |element|
-    b = back || ImmutableStack::empty 
+    b = back || ImmutableStack::empty
     ImmutableQueue.new(front, b.push(element))
   end
 
   pop do
-    f,b = front,back
+    f, b = front, back
     if f == ImmutableStack::empty
       #reverse the back stack to be able to pop from the front in correct order
       until b == ImmutableStack::empty
-        e,b = b.pop
+        e, b = b.pop
         f = f.push(e)
       end
     end
-    head,f = f.pop
+    head, f = f.pop
     if f == b #can only happen if they are both ImmutableStack::empty
-      [head,ImmutableQueue::empty]
+      [head, ImmutableQueue::empty]
     else
-      [head, ImmutableQueue.new(f,b)]
+      [head, ImmutableQueue.new(f, b)]
     end
   end
 
   empty true do
-    @@empty ||= ImmutableQueue.new(ImmutableStack::empty,ImmutableStack::empty)
+    @@empty ||= ImmutableQueue.new(ImmutableStack::empty, ImmutableStack::empty)
   end
 
   push_array do |arr|
     q = self
     if arr
       arr.each do |i|
-       q = q.push i
-     end
+        q = q.push i
+      end
     end
     q
   end
 
   private
 
-  initialize do |front,back|
-    @front = front  || ImmutableStack::empty
+  initialize do |front, back|
+    @front = front || ImmutableStack::empty
     @back = back || ImmutableStack::empty
     self.freeze
   end
